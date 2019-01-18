4792a1825ecb88a23ae43af1e92e527609a97f1c
Avazbek <avazbek.ismanov@gmail.com>
2019-01-18 18:37:19 +0500

    add migration setting for article table

--- migrations/m190118_130412_create_article_table.php
+++ migrations/m190118_130412_create_article_table.php
@@ -14,6 +14,15 @@ class m190118_130412_create_article_table extends Migration
     {
         $this->createTable('article', [
             'id' => $this->primaryKey(),
+            'title' => $this->string(),
+            'description' => $this->text(),
+            'content' => $this->text(),
+            'date' => $this->date(),
+            'image' => $this->string(),
+            'vieved' => $this->integer(),
+            'user_id' => $this->integer(),
+            'status' => $this->integer(),
+            'category_id' => $this->integer(),
         ]);
     }
 
